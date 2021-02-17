623de67ffb57056beb16157db6a86bf52bd41c3f
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-17 23:59:31 +0900

    init

---
+++ src/main/java/com/study/project/springstudy2/Springstudy2Application.java
@@ -0,0 +1,13 @@
+package com.study.project.springstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springstudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springstudy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/project/springstudy2/Springstudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.project.springstudy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springstudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
