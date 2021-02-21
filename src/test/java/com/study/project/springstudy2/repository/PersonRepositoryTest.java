da8bb8d472eb27755fdf9146cb2b447463fa162b
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-21 20:29:34 +0900

    Person repository test

---
+++ src/test/java/com/study/project/springstudy2/repository/PersonRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.study.project.springstudy2.repository;
+
+import com.study.project.springstudy2.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PersonRepositoryTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud(){
+        Person person = new Person();
+        person.setName("martin");
+        person.setAge(10);
+
+        personRepository.save(person);
+        System.out.println(personRepository.findAll());
+        List<Person> people = personRepository.findAll();
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("martin");
+        assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+
+    @Test
+    void hashCodeAndEquals(){
+        Person p1 = new Person("martin", 10);
+        Person p2 = new Person("martin", 10);
+
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+    }
+}
