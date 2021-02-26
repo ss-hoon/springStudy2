49451e1210ba4562af96992594ad86c183f5359f
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-26 23:45:29 +0900

    test data 추가

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`, `job`) values(1, 'martin', 1991, 8, 15, 'programmer');
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values(2, 'david', 1992, 5, 22);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values(3, 'dennis', 1993, 10, 15);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values(4, 'sophia', 1994, 6, 30);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values(5, 'benny', 1995, 8, 29);
